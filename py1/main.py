bdaa99bddd63deb03b6682b026d477dbd006ea1e
Baldur <114744176+BaldurRoethingheim@users.noreply.github.com>
2023-09-15 23:47:03 +0200

    update

--- py1/main.py
+++ py1/main.py
@@ -8,6 +8,9 @@ comment.
 # tutorial "https://www.youtube.com/watch?v=oxXAb8IikHM&list=PL_pqkvxZ6ho3u8PJAsUU-rOAQ74D0TqZB"
 
 # ex_1 Dein erstes Programm
+from os import name
+
+
 print("Hey.\r\n"
       "How are you?")
 def print_hi(name):
@@ -33,4 +36,31 @@ print("""Die
 Programmiersprache "Python" ist cool.""")
 
 # ex_4 Variablen
+num_age = 27
+str_name = "Markus"
+print(num_age)
+num_age = 28
+print(num_age)
+print(str_name)
+
+# ex_5 Datentypen int, float, str
+print("String " + "Verkettung")
+print(10+3)
+# print(10+"String") # Fehler
+int_variable_zahl = 100
+float_variable = 13.734
+str_variable1 = "Das ist ein String."
+str_variable2 = 'Das ist ein String.'
+
+# ex_6 Die input()-Funktion
+print("Angfang des Programms")
+str_name = input("Hinweis: Bitte deinen Namen einngeben:")
+print("Meine Name ist " + str_name)
+num_age = input("Hinweis: Bitte dein Alter einngeben:")
+print("Mein Alter ist " + num_age)
+print("Ende des Programms")
+
+#ex_7 Type-Casting-Funktionen
+
+
 
