1110aabb72de88dbeecedb41406ccfa7fba82f2a
Baldur <114744176+BaldurRoethingheim@users.noreply.github.com>
2023-09-15 23:04:56 +0200

    update_x

--- py1/main.py
+++ py1/main.py
@@ -4,11 +4,20 @@ multi line
 comment.
 '''
 # this is a single line comment
+
+# ex1
 print("Hey.\r\n"
       "How are you?")
-
 def print_hi(name):
-    print(f'Hi, {name}')
-    
+    print(f'Hallo, {name}.')    
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Welt')    
+print("Hallo Welt, ich lerne gerade programmieren.")
+
+# ex2
+print(-7)
+print(8/4)
+print((3+2)*(2-1))
+print(2.1 + 1.5)
+print(2.4*2)
+
