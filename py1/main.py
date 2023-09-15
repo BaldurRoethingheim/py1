88df6ed4428522494f02fc2d158a6eff629cc3a0
Baldur <114744176+BaldurRoethingheim@users.noreply.github.com>
2023-09-15 23:17:43 +0200

    update_x

--- py1/main.py
+++ py1/main.py
@@ -5,7 +5,9 @@ comment.
 '''
 # this is a single line comment
 
-# ex1
+# tutorial "https://www.youtube.com/watch?v=oxXAb8IikHM&list=PL_pqkvxZ6ho3u8PJAsUU-rOAQ74D0TqZB"
+
+# ex_1 Dein erstes Programm
 print("Hey.\r\n"
       "How are you?")
 def print_hi(name):
@@ -14,10 +16,21 @@ if __name__ == '__main__':
     print_hi('Welt')    
 print("Hallo Welt, ich lerne gerade programmieren.")
 
-# ex2
+# ex_2 Zahlen in Python
 print(-7)
 print(8/4)
 print((3+2)*(2-1))
 print(2.1 + 1.5)
 print(2.4*2)
+
+# ex_3 Strings in Python
+print("Hallo Welt")
+print('Hallo '+'Welt.')
+# mehrzeiliger String
+print('''Die 
+Programmiersprache "Python" ist cool.''')
+print("""Die 
+Programmiersprache "Python" ist cool.""")
+
+# ex_4 Variablen
 
