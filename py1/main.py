2f6e3936816d7df8349753ea468d12424d121bb9
Baldur <114744176+BaldurRoethingheim@users.noreply.github.com>
2023-09-20 16:18:23 +0200

    update

--- py1/main.py
+++ py1/main.py
@@ -9,7 +9,7 @@ comment.
 # tutorial "https://www.youtube.com/watch?v=oxXAb8IikHM&list=PL_pqkvxZ6ho3u8PJAsUU-rOAQ74D0TqZB"
 
 # ex_1 Dein erstes Programm
-print("--------------------------")
+print("\r\n\r\n--------------------------")
 print("'# ex_1 Dein erstes Programm'"
 "\r\n")
 
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 print("Hallo Welt, ich lerne gerade programmieren.")
 
 # ex_2 Zahlen in Python
-print("--------------------------")
+print("\r\n\r\n--------------------------")
 print("'# ex_2 Zahlen in Python'"
 "\r\n")
 
@@ -36,7 +36,7 @@ print(2.1 + 1.5)
 print(2.4*2)
 
 # ex_3 Strings in Python
-print("--------------------------")
+print("\r\n\r\n--------------------------")
 print("'# ex_3 Strings in Python'"
 "\r\n")
 
@@ -49,7 +49,7 @@ print("""Die
 Programmiersprache "Python" ist cool.""")
 
 # ex_4 Variablen
-print("--------------------------")
+print("\r\n\r\n--------------------------")
 print("'# ex_4 Variablen'"
 "\r\n")
 
@@ -61,7 +61,7 @@ print(num_age)
 print(str_name)
 
 # ex_5 Datentypen int, float, str
-print("--------------------------")
+print("\r\n\r\n--------------------------")
 print("'# ex_5 Datentypen int, float, str'"
 "\r\n")
 
@@ -74,27 +74,70 @@ str_variable1 = "Das ist ein String."
 str_variable2 = 'Das ist ein String.'
 
 # ex_6 Die input()-Funktion
-print("--------------------------")
+print("\r\n\r\n--------------------------")
 print("'# ex_6 Die input()-Funktion'"
 "\r\n")
 
 print("Angfang des Programms")
-str_name = input("Hinweis: Bitte deinen Namen einngeben:")
+str_name = input("Hinweis: Bitte deinen Namen eingeben:")
 print("Meine Name ist " + str_name)
-num_age = input("Hinweis: Bitte dein Alter einngeben:")
+num_age = input("Hinweis: Bitte dein Alter eingeben:")
 print("Mein Alter ist " + num_age)
 print("Ende des Programms")
 
 # ex_7 Type-Casting-Funktionen
-print("--------------------------")
+print("\r\n\r\n--------------------------")
 print("'# ex_7 Type-Casting-Funktionen'"
 "\r\n")
 
 value1 = input("Erste Zahle eingeben: ")
 value2 = input("Zweite Zahle eingeben: ")
 
+type_of_value = type(value1)
+print(type_of_value)
 print(value1)
+type_of_value = type(value2)
+print(type_of_value)
 print(value2)
+
+print(value1+value2)
+print(int(value1) + int(value2))
+
+value1 = int(value1)
+value2 = int(value2)
+print((value1+value2))
+
+print(int(value1+value2))
+print(float(value1+value2))
+print(str(value1+value2))
+
+# ex_8 Der Zuweisungsoperator
+print("\r\n\r\n--------------------------")
+print("'# ex_8 Der Zuweisungsoperator'"
+"\r\n")
+
+example1 = 22
+
+example2 = 22 + 3
+
+example3 = (22 - 2) * 3
+
+value1 = 5
+value2 = 5
+
+value1 = value1 + 10
+print(value1)
+
+value2 /= 2
+print(value2)
+
+# ex_9 Vergleichsoperatoren und der Datentyp bool
+print("\r\n\r\n--------------------------")
+print("'# ex_9 Vergleichsoperatoren und der Datentyp bool'"
+"\r\n")
+
+
+
 # ...
 
 
