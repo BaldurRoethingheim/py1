5ad97af6e0f4a67a5035819781c8d10eca274528
Baldur <114744176+BaldurRoethingheim@users.noreply.github.com>
2023-09-26 15:15:53 +0200

    update: if, b_exec_all;

--- py1/main.py
+++ py1/main.py
@@ -5,139 +5,191 @@ comment.
 '''
 # this is a single line comment
 
+b_exec_all = False
+false = False  # False bool
+true = True    # True bool
+str_trennlinie = "\r\n\r\n----------------------------------------------------------------------------"
 
 # tutorial "https://www.youtube.com/watch?v=oxXAb8IikHM&list=PL_pqkvxZ6ho3u8PJAsUU-rOAQ74D0TqZB"
 
 # ex_1 Dein erstes Programm
-print("\r\n\r\n--------------------------")
-print("'# ex_1 Dein erstes Programm'"
-"\r\n")
-
 from os import name
-
-
-print("Hey.\r\n"
-      "How are you?")
-def print_hi(name):
-    print(f'Hallo, {name}.')    
-if __name__ == '__main__':
-    print_hi('Welt')    
-print("Hallo Welt, ich lerne gerade programmieren.")
+if (b_exec_all):
+        
+    print(str_trennlinie)
+    print("'# ex_1 Dein erstes Programm'"
+    "\r\n")
+        
+    print("Hey.\r\n"
+          "How are you?")
+    def print_hi(name):
+        print(f'Hallo, {name}.')    
+    if __name__ == '__main__':
+        print_hi('Welt')    
+    print("Hallo Welt, ich lerne gerade programmieren.")
 
 # ex_2 Zahlen in Python
-print("\r\n\r\n--------------------------")
-print("'# ex_2 Zahlen in Python'"
-"\r\n")
-
-print(-7)
-print(8/4)
-print((3+2)*(2-1))
-print(2.1 + 1.5)
-print(2.4*2)
+if (b_exec_all):
+       
+    print(str_trennlinie)
+    print("'# ex_2 Zahlen in Python'"
+    "\r\n")
+    
+    print(-7)
+    print(8/4)
+    print((3+2)*(2-1))
+    print(2.1 + 1.5)
+    print(2.4*2)
 
 # ex_3 Strings in Python
-print("\r\n\r\n--------------------------")
-print("'# ex_3 Strings in Python'"
-"\r\n")
-
-print("Hallo Welt")
-print('Hallo '+'Welt.')
-# mehrzeiliger String
-print('''Die 
-Programmiersprache "Python" ist cool.''')
-print("""Die 
-Programmiersprache "Python" ist cool.""")
+if (b_exec_all):
+    print(str_trennlinie)
+    print("'# ex_3 Strings in Python'"
+    "\r\n")
+    
+    print("Hallo Welt")
+    print('Hallo '+'Welt.')
+    # mehrzeiliger String
+    print('''Die 
+    Programmiersprache "Python" ist cool.''')
+    print("""Die 
+    Programmiersprache "Python" ist cool.""")
 
 # ex_4 Variablen
-print("\r\n\r\n--------------------------")
-print("'# ex_4 Variablen'"
-"\r\n")
-
-num_age = 27
-str_name = "Markus"
-print(num_age)
-num_age = 28
-print(num_age)
-print(str_name)
+if (b_exec_all):
+    print(str_trennlinie)
+    print("'# ex_4 Variablen'"
+    "\r\n")
+    num_age = 27
+    str_name = "Markus"
+    print(num_age)
+    num_age = 28
+    print(num_age)
+    print(str_name)
 
 # ex_5 Datentypen int, float, str
-print("\r\n\r\n--------------------------")
-print("'# ex_5 Datentypen int, float, str'"
-"\r\n")
-
-print("String " + "Verkettung")
-print(10+3)
-# print(10+"String") # Fehler
-int_variable_zahl = 100
-float_variable = 13.734
-str_variable1 = "Das ist ein String."
-str_variable2 = 'Das ist ein String.'
+if (b_exec_all):
+    
+    print(str_trennlinie)
+    print("'# ex_5 Datentypen int, float, str'"
+    "\r\n")
+    
+    print("String " + "Verkettung")
+    print(10+3)
+    # print(10+"String") # Fehler
+    int_variable_zahl = 100
+    float_variable = 13.734
+    str_variable1 = "Das ist ein String."
+    str_variable2 = 'Das ist ein String.'
 
 # ex_6 Die input()-Funktion
-print("\r\n\r\n--------------------------")
-print("'# ex_6 Die input()-Funktion'"
-"\r\n")
-
-print("Angfang des Programms")
-str_name = input("Hinweis: Bitte deinen Namen eingeben:")
-print("Meine Name ist " + str_name)
-num_age = input("Hinweis: Bitte dein Alter eingeben:")
-print("Mein Alter ist " + num_age)
-print("Ende des Programms")
+if (b_exec_all):
+    print(str_trennlinie)
+    print("'# ex_6 Die input()-Funktion'"
+    "\r\n")
+    
+    print("Angfang des Programms")
+    str_name = input("Hinweis: Bitte deinen Namen eingeben:")
+    print("Meine Name ist " + str_name)
+    num_age = input("Hinweis: Bitte dein Alter eingeben:")
+    print("Mein Alter ist " + num_age)
+    print("Ende des Programms")
 
 # ex_7 Type-Casting-Funktionen
-print("\r\n\r\n--------------------------")
-print("'# ex_7 Type-Casting-Funktionen'"
-"\r\n")
-
-value1 = input("Erste Zahle eingeben: ")
-value2 = input("Zweite Zahle eingeben: ")
-
-type_of_value = type(value1)
-print(type_of_value)
-print(value1)
-type_of_value = type(value2)
-print(type_of_value)
-print(value2)
-
-print(value1+value2)
-print(int(value1) + int(value2))
-
-value1 = int(value1)
-value2 = int(value2)
-print((value1+value2))
-
-print(int(value1+value2))
-print(float(value1+value2))
-print(str(value1+value2))
+if (b_exec_all):
+    print(str_trennlinie)
+    print("'# ex_7 Type-Casting-Funktionen'"
+    "\r\n")
+    
+    value1 = input("Erste Zahle eingeben: ")
+    value2 = input("Zweite Zahle eingeben: ")
+    
+    type_of_value = type(value1)
+    print(type_of_value)
+    print(value1)
+    type_of_value = type(value2)
+    print(type_of_value)
+    print(value2)
+    
+    print(value1+value2)
+    print(int(value1) + int(value2))
+    
+    value1 = int(value1)
+    value2 = int(value2)
+    print((value1+value2))
+    
+    print(int(value1+value2))
+    print(float(value1+value2))
+    print(str(value1+value2))
 
 # ex_8 Der Zuweisungsoperator
-print("\r\n\r\n--------------------------")
-print("'# ex_8 Der Zuweisungsoperator'"
-"\r\n")
-
-example1 = 22
-
-example2 = 22 + 3
-
-example3 = (22 - 2) * 3
-
-value1 = 5
-value2 = 5
-
-value1 = value1 + 10
-print(value1)
-
-value2 /= 2
-print(value2)
+if (b_exec_all):
+    print(str_trennlinie)
+    print("'# ex_8 Der Zuweisungsoperator'"
+    "\r\n")
+    
+    example1 = 22
+    
+    example2 = 22 + 3
+    
+    example3 = (22 - 2) * 3
+    
+    value1 = 5
+    value2 = 5
+    
+    value1 = value1 + 10
+    print(value1)
+    
+    value2 /= 2
+    print(value2)
 
 # ex_9 Vergleichsoperatoren und der Datentyp bool
-print("\r\n\r\n--------------------------")
-print("'# ex_9 Vergleichsoperatoren und der Datentyp bool'"
-"\r\n")
+if (b_exec_all):
+    print(str_trennlinie)
+    print("'# ex_9 Vergleichsoperatoren und der Datentyp bool'"
+    "\r\n")
+    
+    print(1 < 5) # (<) kleiner als Operator
+    print(5 < 1)
+    print(1 > 5) # (>) größer als Operator
+    print(5 > 1)
+    
+    print(type(1 < 5))
+    print(type(5 < 1))
+    print(type(1 > 5))
+    print(type(5 > 1))
+    
+    print(1 <= 5) # (<=) kleiner als oder gleich Operator
+    print(5 <= 5)
+    print(6 <= 5)
+    print(1 >= 5) # (<=) größer als oder gleich Operator
+    print(5 >= 5)
+    print(6 >= 5)
+    
+    print(4 != 5) # (!=) ungleich Operator
+    print((4 + 2) * 3 == 5) # (==) gleich Operator    
 
+# ex_10 Die if Anweisung
+if (b_exec_all):
+    print(str_trennlinie)
+    print("'# ex_10 Die if Anweisung'"
+    "\r\n")
+    
+    age = int(input("Bitte gebe dein Alter ein: "))
+    
+    if age  < 18:
+        print("Achtung der Nutzer ist juenger als 18.")
+        print("Ich bin immer noch innerhalb der if-Anweisung")
+        age = 99
+    print("Programmende")
 
-
+# ex_11 if-Anweisung mit elif- und else-Zweigen erweitern
+if (True):
+    print(str_trennlinie)
+    print("'# ex_11 if-Anweisung mit elif- und else-Zweigen erweitern'"
+    "\r\n")    
+    
+    
 # ...
 
 
